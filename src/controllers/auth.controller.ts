443e5dbfe4974f75b6a1793ea88da4278659d3ed
nmpavel <dustopavel@gmail.com>
2025-12-05 00:32:27 +0600

    Initialize the project with basic auth operations

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import User from "../models/User";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken";
+
+export const registerUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Name ,Email & Password fields are required !" });
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists !" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      message: "User registered successfully.",
+      token: generateToken(user._id.toString()),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error", error });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email & Password required !" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    return res.json({
+      message: "Login successful",
+      token: generateToken(user._id.toString()),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error", error });
+  }
+};
---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/auth.routes";
+import { connectDB } from "./core/db";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Log requests
+app.use((req, res, next) => {
+  const originalJson = res.json;
+  res.json = function (body) {
+    console.log(`
+=====================================
+Request:${res.statusCode} ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}
+Response Body: ${JSON.stringify(body)}
+=====================================
+    `);
+    return originalJson.call(this, body);
+  };
+  next();
+});
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
